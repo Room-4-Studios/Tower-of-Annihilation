b43700fd53519c6e18142792e7a0a35aef01f3fb
Kim Huynh <citron.huynh@gmail.com>
2021-02-10 00:21:07 -0800

    Made basic attack script for player
    Made attack script for player, didn't modify blend tree or player direction yet.

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/Attack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float Health;
+    public Animator animator; //Get player's animations.
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Get the players Rigidbody2D
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            //ScriptNameHere nom = collision.GetComponent<ScriptNameHere>();
+
+            //Health -= nom.damage;
+
+            //Destroy(nom.gameObject);
+        }
+    }
+}
