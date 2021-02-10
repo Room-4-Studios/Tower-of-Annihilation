d1bf4ecc9a6e7418b3db1d6a511eab86af581780
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2021-02-10 01:14:03 -0800

    Small update to movement tree
    - Updated movement tree
    - Changed idle samples
    - Added left idle

--- Tower-of-Annihilation/Assets/Nathan/Scripts/PlayerMovement.cs
+++ Tower-of-Annihilation/Assets/Nathan/Scripts/PlayerMovement.cs
@@ -20,6 +20,16 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        // if(movement.sqrMagnitude != 0)
+        // {
+        //     animator.SetFloat("Latitude", movement.x);
+        // }
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
+        {
+            animator.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+        }
     }
 
     void FixedUpdate()
