c446fa5217fa8adbcea64014dc6c888034d73965
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2021-02-15 00:16:42 -0800

    added tileset
    - Added tileset
    - Updated scene
    - Camera Follows character

---
+++ Tower-of-Annihilation/Assets/Nathan/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform player;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
