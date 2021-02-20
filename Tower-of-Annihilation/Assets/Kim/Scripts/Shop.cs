c5f62fc532241f65cd42487a4d6a18ffb8261e90
Kim Huynh <citron.huynh@gmail.com>
2021-02-19 20:41:28 -0800

    Setup shop options

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/Shop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Shop : MonoBehaviour
+{
+    public Sprite smallHealthPotion;
+    public Sprite bigHealthPotion;
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        CreateItemButton(smallHealthPotion, "Small Health Potion", 10, 0);
+        CreateItemButton(bigHealthPotion, "Big Health Potion", 40, 1);
+    }
+
+
+    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+        float shopItemHeight = 70f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        shopItemTransform.Find("itemDesc").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("itemCost").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+        shopItemTransform.Find("itemImag").GetComponent<Image>().sprite = itemSprite;
+    }
+}
