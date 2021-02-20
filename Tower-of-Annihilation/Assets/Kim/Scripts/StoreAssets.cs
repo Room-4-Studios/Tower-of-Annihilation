7687d0d1796610c8fb5cc04407dee938b131422a
Kim Huynh <citron.huynh@gmail.com>
2021-02-19 18:56:35 -0800

    Added basic prefab for shop.

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/StoreAssets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreAssets : MonoBehaviour
+{
+    private static StoreAssets _i;
+
+    public static StoreAssets i //instance
+    {
+        get {
+            if (_i == null) _i = (Instantiate(Resources.Load("StoreAssets")) as GameObject).GetComponent<StoreAssets>();
+            return _i;
+        }
+    }
+
+    public Sprite smallHealthPotion;
+    public Sprite bigHealthPotion;
+
+}
