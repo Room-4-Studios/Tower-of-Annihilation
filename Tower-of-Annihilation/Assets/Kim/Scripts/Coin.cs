73917ea4daf8828459243ab95ed59b23f88d8cdc
Kim Huynh <citron.huynh@gmail.com>
2021-02-21 13:52:16 -0800

    Re-added coin prefab, anim

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public GameObject mngr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mngr = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mngr.SendMessage("money");
+            Destroy(gameObject);
+        }
+    }
+}
