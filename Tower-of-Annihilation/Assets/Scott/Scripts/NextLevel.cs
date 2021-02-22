8fb912db8390831806690b139d9632896dbe9785
Scott Martin <78115939+scjmartin@users.noreply.github.com>
2021-02-21 20:07:42 -0800

    Script name changed

--- Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
+++ Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Level2 : MonoBehaviour
+public class NextLevel : MonoBehaviour
 {
-    /* when player and collider at top of stairs are touching and "Return" key is
-     * pressed the next level will be initiated. */
+    /* when player and collider at top of stairs are touching 
+     * the next level will be initiated. */
     private void OnCollisionStay2D(Collision2D Collision)
     {
         if (Collision.gameObject.name == "NLCollider")
         {
-             SceneManager.LoadScene(2);
+             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
