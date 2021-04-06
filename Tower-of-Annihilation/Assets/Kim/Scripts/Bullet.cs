a14b2c8505b58757c2a0d957712ad66091dade9f
Kim Huynh <citron.huynh@gmail.com>
2021-04-05 21:39:03 -0700

    Added basic boss pattern+scene
    Yeah, I couldn't find a pixel dragon...

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    private float moveSpeed;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/BulletPool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=Mq2zYk5tW_E
+
+public class BulletPool : MonoBehaviour
+{
+    public static BulletPool bulletPoolInstance;
+
+    [SerializeField]
+    private GameObject pooledBullet;
+    private bool notEnoughBullets = true;
+
+    private List<GameObject> bullets;
+
+    private void Awake()
+    {
+        bulletPoolInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullets = new List<GameObject>();
+    }
+
+    public GameObject GetBullet()
+    {
+        if (bullets.Count > 0)
+        {
+            for(int i=0; i<bullets.Count; i++)
+            {
+                if (!bullets[i].activeInHierarchy)
+                {
+                    return bullets[i];
+                }
+            }
+        }
+
+        if (notEnoughBullets)
+        {
+            GameObject bul = Instantiate(pooledBullet);
+            bul.SetActive(false);
+            bullets.Add(bul);
+            return bul;
+        }
+
+        return null;
+    }
+
+
+}
