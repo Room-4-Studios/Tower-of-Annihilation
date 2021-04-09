f52343c7ee9064fc3ff8d633d3f150244b4b7c97
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2021-04-08 20:44:42 -0700

    Medium update
    added player health bar
    changes to level 2
    added store stair prefab

--- Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
+++ Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    private int randLevel;
     /* when player and collider at top of stairs are touching 
      * the next level will be initiated. */
     private void OnCollisionEnter2D(Collision2D Collision)
@@ -15,7 +16,16 @@ public class NextLevel : MonoBehaviour
             //Debug.Log("Henlo");
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); We need to look into this, with more random scenes.
             /* Scene changes */
-            SceneManager.LoadScene("Level 1");
+            randLevel = Random.Range(2,4); 
+            SceneManager.LoadScene(randLevel);
+        }
+
+        else if (Collision.gameObject.name == "ShopCollider")
+        {
+            //Debug.Log("Henlo");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); We need to look into this, with more random scenes.
+            /* Scene changes */
+            SceneManager.LoadScene("ShopKeeper Level");
         }
     }
 }
