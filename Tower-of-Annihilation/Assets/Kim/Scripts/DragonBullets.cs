b961dba5a6f4dd725080f57e8f3b00422f601fea
Kim Huynh <citron.huynh@gmail.com>
2021-04-15 01:18:25 -0700

    Added new bullet patterns for boss.

--- Tower-of-Annihilation/Assets/Kim/Scripts/DragonBullets.cs
+++ Tower-of-Annihilation/Assets/Kim/Scripts/DragonBullets.cs
@@ -9,16 +9,20 @@ public class DragonBullets : MonoBehaviour
 
     [SerializeField]
     private float startAngle = 90f, endAngle = 270f;
+    private float angle2 = 0f; //angle for pattern 2
+    private float angle3 = 0f; //angle for pattern 3
 
     private Vector2 bulletDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Fire", 0f, 2f); //Fire every two seconds.
+        InvokeRepeating("Fire1", 0f, 2f); //Fire every two seconds.
+        InvokeRepeating("Fire2", 0f, 0.1f);
+        InvokeRepeating("Fire3", 0f, 0.1f);
     }
 
-    private void Fire()
+    private void Fire1() //Pattern 1, straight lines.
     {
         float angleStep = (endAngle - startAngle) / bulletAmount;
         float angle = startAngle;
@@ -41,6 +45,46 @@ public class DragonBullets : MonoBehaviour
         }
     }
 
+    private void Fire2() //Pattern 2, spiral pattern.
+    {
+        float bulDirX = transform.position.x + Mathf.Sin((angle2 * Mathf.PI) / 180f);
+        float bulDirY = transform.position.y + Mathf.Cos((angle2 * Mathf.PI) / 180f);
+
+        Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+        Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+        GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+        bul.transform.position = transform.position;
+        bul.transform.rotation = transform.rotation;
+        bul.SetActive(true);
+        bul.GetComponent<Bullet>().SetDirection(bulDir);
+
+        angle2 += 10f;       
+    }
+
+    private void Fire3() //Pattern 3, double spiral.
+    {
+
+        for (int i = 0; i <=1; i++)
+        {
+            float bulDirX = transform.position.x + Mathf.Sin((angle3 * Mathf.PI) / 180f);
+            float bulDirY = transform.position.y + Mathf.Cos((angle3 * Mathf.PI) / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetDirection(bulDir);
+        }
+        angle3 += 10f;
+        if (angle3 >= 360f)
+        {
+            angle3 = 0f;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
