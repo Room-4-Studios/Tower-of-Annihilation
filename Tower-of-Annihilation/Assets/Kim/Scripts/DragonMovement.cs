710f458521b027c3c732f39e7e46e3a8105dd636
Kim Huynh <citron.huynh@gmail.com>
2021-04-15 21:01:04 -0700

    The dragon now moves.

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/DragonMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonMovement : MonoBehaviour
+{
+    private float moveSpeed;
+    private bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 2f;
+        moveRight = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 4f)
+        {
+            moveRight = false;
+        }
+        else if (transform.position.x < -4f)
+        {
+            moveRight = true;
+        }
+        if (moveRight)
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
