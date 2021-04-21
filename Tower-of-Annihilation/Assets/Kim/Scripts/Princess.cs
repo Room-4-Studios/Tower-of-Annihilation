9ef54b9df0957c407b139ac4d0d684a193d63a44
Kim Huynh <citron.huynh@gmail.com>
2021-04-21 15:28:28 -0700

    Edited Princess, made Singleton of her.

---
+++ Tower-of-Annihilation/Assets/Kim/Scripts/Princess.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Example of Singleton pattern.
+ * Ensure a class has only one instance, and provide a global point of access to it.
+ * Encapsulated "just-in-time initialization" or "initialization on first use".
+ * 
+ */
+
+public class Princess : MonoBehaviour
+{
+    private static Princess _instance;
+    public static Princess Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Reached Princess");
+            SceneManager.LoadScene("Ending 2");
+        }
+    }
+}
