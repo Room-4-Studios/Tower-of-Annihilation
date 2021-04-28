ba1ebe593d0bbdaf31ed9443cfea4e675acb6779
Scott Martin <78115939+scjmartin@users.noreply.github.com>
2021-04-28 00:04:34 -0700

    New level, changed next level script
    cannot start next level until all enemies are dead
    levels wont repeat (spotty still working on this)
    Added a new level

--- Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
+++ Tower-of-Annihilation/Assets/Scott/Scripts/NextLevel.cs
@@ -6,21 +6,48 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     private int randLevel;
+    private int[] levelArr = {0, 0, 0};
+    private int NLColliderTouches = 0;
+
     /* when player and collider at top of stairs are touching 
      * the next level will be initiated. */
     private void OnCollisionEnter2D(Collision2D Collision)
     {
-        /* If the player touches the collider, if statement is called */
-        if (Collision.gameObject.name == "NLCollider")
+        /* If the player touches the collider and all enemies are eliminated, if statement is called */
+        if (Collision.gameObject.name == "NLCollider" && (GameObject.Find("Slime(Clone)") == null))
         {
             //Debug.Log("Henlo");
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); We need to look into this, with more random scenes.
             /* Scene changes */
-            randLevel = Random.Range(2,4); 
-            SceneManager.LoadScene(randLevel);
+            NLColliderTouches++;
+            randLevel = Random.Range(2,5); 
+            for(int i = 0; i <= 2; i++)
+            {
+                if(randLevel == levelArr[i])
+                {
+                    randLevel = Random.Range(2, 5);
+                    i = 0;
+                }
+            }
+            for(int i = 0; i <= 2; i++)
+            {
+                if(levelArr[i] == 0)
+                {
+                    levelArr[i] = randLevel;
+                    SceneManager.LoadScene(randLevel);
+                    break;
+                }
+            }
+            if(NLColliderTouches == 4)
+            {
+                SceneManager.LoadScene("Boss Scene");
+            }
+            
+            //Debug.Log("Arr contents; " + levelArr[0] + ", " + levelArr[1] + ", " + levelArr[2] + "\n");
+           
         }
 
-        else if (Collision.gameObject.name == "ShopCollider")
+        else if (Collision.gameObject.name == "ShopCollider" && (GameObject.Find("Slime(Clone)") == null))
         {
             //Debug.Log("Henlo");
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); We need to look into this, with more random scenes.
