fe532fa5bc8e80b8458147eb2b4711bc77af151c
Dawson Hill <extremedawson@gmail.com>
2021-05-05 23:52:34 -0700

    Pause Menu Functionality

---
+++ Tower-of-Annihilation/Assets/Dawson/SettingsMenu.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void BcMode(bool _isOn)
+    {
+        Debug.Log("HELLO");
+    }
+}
